26fcea133715c7d62b5d9fd4ad09699ad74ac092
TheKachi <nzebenflorence@gmail.com>
2019-09-03 18:37:23 +0100

    completed development

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Clarifai from 'clarifai'
 import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo'
@@ -10,10 +9,6 @@ import SignIn from './components/SignIn/SignIn';
 import Register from './components/Register/Register';  
 import './App.css';
 
-const app = new Clarifai.App({
-  apiKey: '91cd6a1de3be4c7885c9673b59bbcf3e'
- });
- 
 const particlesOptions = {
   particles: {
     number: {
@@ -26,23 +21,25 @@ const particlesOptions = {
   }
 }
 
+const initialState = {
+  input: '',
+  imageUrl: '',
+  box: {},
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+  }
+}
+
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      input: '',
-      imageUrl: '',
-      box: {},
-      route: 'signin',
-      isSignedIn: false,
-      user: {
-        id: '',
-        name: '',
-        email: '',
-        entries: 0,
-        joined: ''
-      }
-    }
+    this.state = initialState;
   }
 
   loadUser = (data) => {
@@ -76,12 +73,16 @@ class App extends Component {
     this.setState({input: event.target.value});
   }
 
-  onPictureSubmit = () => {
-    this.setState({ imageUrl: this.state.input });
-    app.models
-      .predict(
-        Clarifai.FACE_DETECT_MODEL,
-        this.state.input)
+  onButtonSubmit = () => {
+    this.setState({ imageurl: this.state.input });
+    fetch('http://localhost:3000/imageurl', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        input: this.state.input
+      })
+    })
+      .then(response => response.json())
       .then(response => {
         if (response) {
           fetch('http://localhost:3000/image', {
@@ -101,10 +102,10 @@ class App extends Component {
       })
       .catch(err => console.log(err));
   }
-
+  
   onRouteChange = (route) => {
     if (route === 'signout') {
-      this.setState({isSignedIn: false})
+      this.setState(initialState)
     } else if (route === 'home') {
       this.setState({isSignedIn: true})
     }
@@ -118,22 +119,19 @@ class App extends Component {
       <Particles className='particles'
         params={particlesOptions}
       />
-      <Navigation onRouteChange={this.onRouteChange} isSignedIn={isSignedIn}/>
+      <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
       { route === 'home' 
           ? <div> 
               <Logo />     
               <Rank name={this.state.user.name} entries={this.state.user.entries} /> 
               <ImageLinkForm 
                 onInputChange={this.onInputChange} 
-                onButtonSubmit={this.onPictureSubmit}/>
-              <FaceRecognition imageUrl={imageUrl} box={box} />   
+                onButtonSubmit={this.onButtonSubmit}/>
+              <FaceRecognition box={box} imageUrl={imageUrl}/>   
             </div>
         : (
             route === 'signin'      
-              ? <div>
-                  <Logo /> 
-                  <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
-                </div>
+              ? <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
               : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>               
         )    
       }
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./ImageLinkForm.css";
 
-const ImageLinkForm = ({ onInputChange, onPictureSubmit }) => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit}) => {
     return (
         <div>
             <p className='f3'>
@@ -10,9 +10,8 @@ const ImageLinkForm = ({ onInputChange, onPictureSubmit }) => {
             <div className='center'>
                 <div className='form center pa4 br3 shadow-5'>
                    <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange}/>
-                    <button className='w-30 br4 grow f4 link ph3 pv2 dib white bg-light-purple' onClick={onPictureSubmit}>Detect</button> 
-                </div>
-                
+                    <button className='w-30 br4 grow f4 link ph3 pv2 dib white bg-light-purple' onClick={onButtonSubmit}>Detect</button> 
+                </div>        
             </div>
         </div>
         
